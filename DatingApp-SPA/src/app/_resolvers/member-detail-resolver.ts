124a0ab67f2917ece8b2191a654e6bd3d4240b4b
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2020-01-02 22:55:01 +0200

    Paging implementation added

--- DatingApp-SPA/src/app/_resolvers/member-detail-resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-detail-resolver.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { User } from "../_models/user";
-import { Resolve, Router, ActivatedRouteSnapshot } from "@angular/router";
-import { UserService } from "../_services/user.service";
-import { AlertifyService } from "../_services/alertify.service";
-import { Observable, of } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberDetailResolver implements Resolve<User> {
